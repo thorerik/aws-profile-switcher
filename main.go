3adcb97d73c9ec005d7c2858d7bca39a31893f0a
Thor Erik Lie <thor@thorerik.com>
2023-08-24 15:47:38 +0200

    Add Profile switching

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func help() {
 	fmt.Println("Usage: aws-profile [options]")
 	fmt.Println("Options:")
 	fmt.Println("  -l list all profiles")
+	fmt.Println("  -s <profile> set profile")
 	fmt.Println("  -p <profile> print profile")
 	fmt.Println("  -d <profile> delete profile")
 	fmt.Println("  -a <profile> <accsess key id> <secret access key> add profile")
@@ -22,6 +23,13 @@ func getPath() string {
 	homeDir, _ := os.UserHomeDir()
 
 	return homeDir + "/.aws/credentials"
+}
+
+func getConfigPath() string {
+	// Get the home directory path
+	homeDir, _ := os.UserHomeDir()
+
+	return homeDir + "/.aws/config"
 }
 
 func readCredentialsIni() *ini.File {
@@ -43,14 +51,42 @@ func readCredentialsIni() *ini.File {
 	return cfg
 }
 
+func readConfigIni() *ini.File {
+	// Get the path to the config file
+	path := getConfigPath()
+
+	// Check if .aws/config exists
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		fmt.Println("File does not exist")
+	}
+
+	// Load the file
+	cfg, err := ini.Load(path)
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	return cfg
+}
+
 func listProfiles() {
 	cfg := readCredentialsIni()
+	config := readConfigIni()
 
 	// Get the section names
 	sections := cfg.SectionStrings()
 
+	// Get current active profile
+	profile := config.Section("default").Key("profile").String()
+
 	// Print the section names
 	for _, section := range sections {
+		if section == profile {
+			fmt.Print("* ")
+		} else {
+			fmt.Print("  ")
+		}
 		fmt.Println(section)
 	}
 }
@@ -107,9 +143,31 @@ func addProfile(profile string, awsAccessKeyID string, awsSecretAccessKey string
 	}
 }
 
+func setProfile(profile string) {
+	cfg := readCredentialsIni()
+	config := readConfigIni()
+
+	// Get the section
+	_, err := cfg.GetSection(profile)
+	if err != nil {
+		fmt.Printf("Fail to get section: %v", err)
+		os.Exit(1)
+	}
+
+	config.Section("default").Key("profile").SetValue(profile)
+
+	// Save the file
+	err = config.SaveTo(getConfigPath())
+	if err != nil {
+		fmt.Printf("Fail to save file: %v", err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	// parse switches
 	// -l list all profiles
+	// -s <profile> set profile
 	// -p <profile> print profile
 	// -d <profile> delete profile
 	// -a <profile> <accsess key id> <secret access key> add profile
@@ -123,6 +181,12 @@ func main() {
 		switch arg {
 		case "-l":
 			listProfiles()
+		case "-s":
+			if len(os.Args) < i+2 {
+				help()
+				os.Exit(1)
+			}
+			setProfile(os.Args[i+1])
 		case "-p":
 			if len(os.Args) < i+2 {
 				help()
